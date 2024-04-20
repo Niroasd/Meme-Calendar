8efb124c492a5d884a70d9af32290d9604e82ed0
qkas <kassuvaa@gmail.com>
2024-04-20 14:13:24 +0300

    Removed unused useState import

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
 import './App.css'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import MainContent from './components/MainContent'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="app">
       <Header />
